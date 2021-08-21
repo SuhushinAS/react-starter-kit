43b2aee1138fc49f44d7bac25f278ce9b2f7ed5c
SuhushinAS <suhushinas@gmail.com>
2021-08-21 15:39:59 +0700

    fix

--- config/style.js
+++ config/style.js
@@ -3,6 +3,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = ({mode, root}) => {
     const isProd = 'production' === mode;
     const styleLoader = isProd ? MiniCssExtractPlugin.loader : 'style-loader';
+    const cssLoader = {loader: 'css-loader', options: {esModule: false}};
     const plugins = isProd ? [new MiniCssExtractPlugin({filename: '[name].min.css'})] : [];
 
     return {
@@ -10,11 +11,11 @@ module.exports = ({mode, root}) => {
             rules: [
                 {
                     test: /\.css$/u,
-                    use: [styleLoader, 'css-loader', 'postcss-loader'],
+                    use: [styleLoader, cssLoader, 'postcss-loader'],
                 },
                 {
                     test: /\.less$/u,
-                    use: [styleLoader, 'css-loader', 'postcss-loader', {loader: 'less-loader', options: {lessOptions: {paths: [root]}}}],
+                    use: [styleLoader, cssLoader, 'postcss-loader', {loader: 'less-loader', options: {lessOptions: {paths: [root]}}}],
                 },
             ],
         },
