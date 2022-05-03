7cde7205c90ef294585b063a00356cc51f6310ba
SuhushinAS <suhushinas@gmail.com>
2022-05-03 17:51:38 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -3,45 +3,61 @@ import {store} from 'app/store';
 import {ConfigContainer} from 'modules/config/components/Config';
 import {LocaleProviderContainer} from 'modules/locale/components/LocaleProvider';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot, Root} from 'react-dom/client';
 import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
 import 'styles/index.less';
 
-const root = document.getElementById('root');
+/**
+ * Рендер приложения.
+ * @param root {Root} Корень.
+ * @return void.
+ */
+const getRender = (root: Root) => () => {
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <LocaleProviderContainer>
+          <BrowserRouter>
+            <ConfigContainer>
+              <App />
+            </ConfigContainer>
+          </BrowserRouter>
+        </LocaleProviderContainer>
+      </Provider>
+    </React.StrictMode>
+  );
+};
+
+const container = document.getElementById('root');
+
+if (container) {
+  const root = createRoot(container);
+  const render = getRender(root);
+
+  render();
+
+  if (module.hot) {
+    module.hot.accept('app/components/App', render);
+  }
+}
 
 /**
- * Вывести приложение.
+ * По ошибке регистрации.
+ * @param error Ошибка.
  */
-function renderApp() {
-  if (root) {
-    ReactDOM.render(
-      <React.StrictMode>
-        <Provider store={store}>
-          <LocaleProviderContainer>
-            <BrowserRouter>
-              <ConfigContainer>
-                <App />
-              </ConfigContainer>
-            </BrowserRouter>
-          </LocaleProviderContainer>
-        </Provider>
-      </React.StrictMode>,
-      root
-    );
-  }
-}
+const onRegisterError = (error) => {
+  console.error('SW registration failed: ', error);
+};
 
-renderApp();
-
-if (module.hot) {
-  module.hot.accept('app/components/App', renderApp);
-}
-
-window.addEventListener('load', () => {
+/**
+ * По загрузке window.
+ */
+const onWindowLoad = () => {
   if ('serviceWorker' in navigator && navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js').catch((registrationError) => {
-      console.error('SW registration failed: ', registrationError);
-    });
+    navigator.serviceWorker.register('/sw.js').catch(onRegisterError);
   }
-});
+};
+
+window.removeEventListener('load', onWindowLoad);
+window.addEventListener('load', onWindowLoad);
